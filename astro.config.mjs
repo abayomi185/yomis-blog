5f67591238e9853224bf7a9e2203349e5626c2c1
Yomi Ikuru <captyomjnr@gmail.com>
2025-10-04 20:10:21 +0100

    feat: update astro config with expressive code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,44 +1,25 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import { remarkReadingTime } from './src/utils/readingTime';
-import rehypePrettyCode from 'rehype-pretty-code';
 import vercelStatic from '@astrojs/vercel';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
-const options = {
-  // Specify the theme to use or a custom theme json, in our case
-  // it will be a moonlight-II theme from
-  // https://github.com/atomiks/moonlight-vscode-theme/blob/master/src/moonlight-ii.json
-  // Callbacks to customize the output of the nodes
-  //theme: json,
-  onVisitLine(node) {
-    // Prevent lines from collapsing in `display: grid` mode, and
-    // allow empty lines to be copy/pasted
-    if (node.children.length === 0) {
-      node.children = [
-        {
-          type: 'text',
-          value: ' '
-        }
-      ];
-    }
-  },
-  onVisitHighlightedLine(node) {
-    // Adding a class to the highlighted line
-    node.properties.className = ['highlighted'];
-  }
-};
+import expressiveCode from 'astro-expressive-code';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://yomis.blog/',
-  markdown: {
-    // syntaxHighlight: true,
-    rehypePlugins: [[rehypePrettyCode, options]],
-    remarkPlugins: [remarkReadingTime]
-  },
-  integrations: [tailwind(), react(), sitemap(), mdx()],
+  integrations: [
+    tailwind(),
+    react(),
+    sitemap(),
+    expressiveCode({
+      themes: ['github-light', 'github-dark'],
+      useDarkModeMediaQuery: false,
+      themeCssSelector: (theme) => `[class~='dark']`
+    }),
+    mdx()
+  ],
   output: 'static',
   adapter: vercelStatic({
     webAnalytics: {
